f5cf2b03d286f30c9ab32bfc2a4273ca65b33474
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-10-27 15:07:11 -0400

    exercise 3 revision

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,9 @@
 puts "What's your name?"
 name = gets.chomp
 puts "Hi #{name}"
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "You are #{age} years old"
+current_year = 2015
+birth_year = current_year - age
+puts "You were born in #{birth_year}"
