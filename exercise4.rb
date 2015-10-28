1f01cbfaeaada8451466d8fbf6ca0a7423be70af
SCOTT WALSH <SCMWALSH@GMAIL.COM>
2015-10-28 14:59:10 -0400

    improvements to exercise4, lecture exercise

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,11 @@
 number = (1..100)		
 number.each do |number|
 
-	if number %3 == 0 && number %5 == 0
+	if number%3 == 0 && number%5 == 0
 		puts "Bitmaker"
-	elsif number %3 == 0
+	elsif number%3 == 0
 		puts "Bit"
-	elsif number %5 == 0
+	elsif number%5 == 0
 		puts "Maker"
 	
 	else 
---
+++ methods-deux.rb
@@ -0,0 +1,20 @@
+# number1 = 10
+# number2 = 20	
+# result = number1 + number 2
+
+def sum(first, second)
+# 	# result = first + second
+# 	# puts "#{result} is the sum"	
+# 	# result
+return first + second
+# 	# first + second
+	"#{first + second} is the sum"
+end
+
+def add10(first, second = 10)
+	first + second
+end
+
+
+
+
